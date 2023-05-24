4dd232ec6215bfb91f0ae5030ddca3a0d399f9ff
LizaTaff <taffarelliza@gmail.com>
2023-05-23 23:32:42 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function showAlert() { alert('¡Mirá como aprendí a poner un botón de alerta!'); }
+
+const form = document.getElementById('myForm');
+if (form){
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        validateForm();
+    })    
+}
+
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+}
+
+function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+        alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+        alert('Correo electrónico enviado correctamente.');
+    }
+}
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+        document.querySelector(".nav-links").
+        classList.toggle("nav-links-responsive")})
